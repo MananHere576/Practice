ef415e440da3e0216edf46f889b491cc356fc31e
MananHere576 <mananmishra576@gmail.com>
2026-06-13 09:26:21 +0530

    Day3 Practice

---
+++ src/tcs_assessments/Ques4.java
@@ -0,0 +1,21 @@
+package tcs_assessments;
+import java.util.Scanner;
+public class Ques4 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        int s=0,c=0;
+        for (int i = 0; i <str.length() ; i++) {
+            char ch=str.charAt(i);
+            if(ch==' ')
+            {
+                s++;
+            }
+            else if(ch>='a' && ch<='z' || ch>='A' && ch<='Z')
+            {
+                c++;
+            }
+        }
+        System.out.println(s+" "+c);
+    }
+}
