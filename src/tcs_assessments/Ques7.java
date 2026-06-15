60325dc6a207d4d57e594731891fdd03bf598263
MananHere576 <mananmishra576@gmail.com>
2026-06-15 08:40:43 +0530

    Day5 Practice

---
+++ src/tcs_assessments/Ques7.java
@@ -0,0 +1,18 @@
+package tcs_assessments;
+import java.util.Scanner;
+public class Ques7 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        String nstr[]=str.split(" ");
+        String f="";
+        for (int i = 0; i < nstr.length ; i++) {
+            char ch=nstr[i].charAt(0);
+            if(ch=='a' || ch=='e' || ch=='o' || ch=='i' || ch=='u' || ch=='A' || ch=='E' || ch=='O' || ch=='I' || ch=='U')
+            {
+                f=f+ch;
+            }
+        }
+        System.out.println(f);
+    }
+}
