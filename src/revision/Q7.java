573fd7025ff3c3950063c51bd6a3910c5b6d721c
MananHere576 <mananmishra576@gmail.com>
2026-06-22 07:57:21 +0530

    revision

---
+++ src/revision/Q7.java
@@ -0,0 +1,27 @@
+package revision;
+
+import java.util.Scanner;
+
+public class Q7 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int count = 0;
+
+        while (n > 0) {
+
+            int r = n % 10;
+
+            if (r == 2 || r == 3 || r == 5 || r == 7) {
+                count++;
+            }
+
+            n = n / 10;
+        }
+
+        System.out.println(count);
+    }
+}
