47ee6456339086934a239e752f58cbfb98704742
MananHere576 <mananmishra576@gmail.com>
2026-06-29 10:30:22 +0530

    revision

---
+++ src/revision/rev3.java
@@ -0,0 +1,10 @@
+package revision;
+import java.util.Scanner;
+public class rev3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String arr[]=str.split(" ");
+        System.out.println("The Number of Words are:"+arr.length);
+    }
+}
